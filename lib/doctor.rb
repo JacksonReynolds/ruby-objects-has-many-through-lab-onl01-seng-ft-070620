0bbe95f4bc388747ac1ec1696c6f8a585f6e846d
Jackson Reynolds <jwreynold@gmail.com>
2020-07-21 02:32:56 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,19 @@
+require 'patient.rb'
+require 'appointment.rb'
+
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end #initialize
+
+  def self.all
+    @@all
+  end #self.all
+
+end #doctor
